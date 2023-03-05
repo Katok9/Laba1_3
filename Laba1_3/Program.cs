d471d2544437f97e1fa96a26acaf48a83ccdfb60
Катерина Шевченко <katya14shevchenko@gmail.com>
2023-03-05 19:38:20 +0200

    Добавьте файлы проекта.

---
+++ Laba1_3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Laba_1_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            int D = 7;
+            int[] A = { 12, 23, 34, 45, 57 };
+
+            // => - лямбда вираз
+            int result = A.FirstOrDefault(number => number > 0 && number % 10 == D);
+            Console.WriteLine(result);
+
+            //test
+        }
+    }
+}
